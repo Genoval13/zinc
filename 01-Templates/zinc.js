5a638361660d8cd3327b52ed7a6b6f2ea71250a1
Bryan Anderson <bryan.texasbobcat@gmail.com>
2018-10-25 14:29:09 -0500

    01 complete

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -4,33 +4,18 @@
 
 (() => {
 
-    function renderTemplate(HTML, data) {
-        return HTML.replace(/\{\{\s*(\w+)\s*\}\}/g, function (matches, p1) {
-            return data[p1];
-        })
+    function renderTemplate(URL, data) {
+        return fetch(`${URL}.html`)
+            .then(res => res.text())
+            .then(text =>{ 
+                return text.replace(/\{\{\s*(.+?)\s*\}\}/g,  (matches, p1) => 
+            p1.split('.').reduce((acc, cur) => acc[cur], data))})
     }
-
-    let userHTML = `<li class="user">
-        <img class="user-photo" src="{{ photo }}" alt="Photo of {{ firstName }} {{ lastName }}">
-        <div class="user-name">{{ firstName }} {{ lastName }}</div>
-        <div class="user-location">{{ city }}, {{ state }}</div>
-        <div class="user-email">{{ email }}</div>
-        </li>`;
-
+    
     function populateList(results) {
         for (let i = 0; i < results.length; i++) {
-            
-            const data = {
-                photo: results[i].picture.thumbnail,
-                firstName: results[i].name.first,
-                lastName: results[i].name.last,
-                city: results[i].location.city,
-                state: results[i].location.state,
-                email: results[i].email
-            };
-            
-            let template = renderTemplate(userHTML, data);
-            document.getElementById("z-user-list").insertAdjacentHTML("beforeend", template);
+            renderTemplate('user', results[i])
+            .then(template => document.getElementById("z-user-list").insertAdjacentHTML("beforeend", template))
         }
     }
 
