aa425f18cb7863cbc899148dd89229cdf2d33929
Bryan Anderson <bryan.texasbobcat@gmail.com>
2018-10-26 11:18:16 -0500

    2.1 finished

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -5,8 +5,31 @@
 const Zinc = {};
 
 (() => {
+    const userData = {
+        picture: {
+            thumbnail: 'https://f4.bcbits.com/img/0001142378_10.jpg'
+        },
+        name: {
+            first: 'Jack',
+            last: 'Burton'
+        },
+        location: {
+            city: 'San Francisco',
+            state: 'CA'
+        },
+        email: 'jack.burton@example.com'
+    };
+
     function renderComponent(element, content, userData) {
-        console.log(element, content); // eslint-disable-line no-console
+        return fetch(`${content}.html`)
+            .then(res => res.text())
+            .then(text =>{ 
+                return text.replace(/\{\{\s*(.+?)\s*\}\}/g,  (matches, p1) => 
+                    p1.split('.').reduce((acc, cur) => acc[cur], userData))
+            })
+            .then(template => {
+                document.querySelector(element).insertAdjacentHTML("beforeend", template)
+            })
     }
 
     function init() {
